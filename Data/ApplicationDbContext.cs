44b4f06ea096fca1ecfad2da8ef41fc0893dc28f
Your Namenbehina <nbehina@gmail.com>
2020-05-31 11:39:03 -0600

    Modified the ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FlowerStore.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,6 @@ namespace FlowerStore.Data
             : base(options)
         {
         }
+        public DbSet<AdminListItem> AdminListItem { get; set; }
     }
 }
