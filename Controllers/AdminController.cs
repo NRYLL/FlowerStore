f276854458f8b292a432b2654184ddf08236b6c8
Your Namenbehina <nbehina@gmail.com>
2020-05-31 10:24:05 -0600

    Added AdminController.cs

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FlowerStore.Models;
+
+
+namespace FlowerStore.Controllers
+{
+    public class AdminController : Controller
+
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Adminstration()
+        {
+            return View(new Models.AdminListItem());
+        }
+    }
+}
