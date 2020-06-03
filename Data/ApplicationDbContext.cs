3d2de705d533825c665ebde011f12b79b4ae597a
vincentcui5296 <vincentcui5296@gmail.com>
2020-06-03 00:37:23 -0600

    Generate Flower Controller

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FlowerStore.Models;
 
 namespace FlowerStore.Data
 {
@@ -12,5 +13,6 @@ namespace FlowerStore.Data
             : base(options)
         {
         }
+        public DbSet<FlowerStore.Models.Flower> Flower { get; set; }
     }
 }
