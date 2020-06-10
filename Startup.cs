bae98be96ff99a9bd3b5272d4a241e01eb2e5a98
vincentcui5296 <vincentcui5296@gmail.com>
2020-06-10 00:27:21 -0600

    disable register comfirmation

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace FlowerStore
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddAuthentication().AddFacebook(facebookOptions =>
             {
